ac1cd63c00b1c1c9a58493201de8123a2d931715
user69gd <loganlloyd2374747474743747@gmail.com>
2025-09-15 15:47:49 -0500

    Update text_editor.h

--- src/editor/text_editor.h
+++ src/editor/text_editor.h
@@ -1,18 +1,19 @@
-// src/editor/text_editor.h
 #pragma once
 
 #include <QPlainTextEdit>
 #include <QTimer>
+#include "language_support/syntax_highlighter.h"
 
 class TextEditor : public QPlainTextEdit {
     Q_OBJECT
 public:
     explicit TextEditor(QWidget *parent = nullptr);
-    void setLanguageMode(const QString& language);
+    void openFile(const QString& filePath);
 
 private slots:
     void autoSave();
 
 private:
     QTimer m_autoSaveTimer;
+    SyntaxHighlighter* m_highlighter; // Base class for all highlighters
 };
