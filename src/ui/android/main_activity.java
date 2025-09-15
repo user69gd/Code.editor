0ebce2e87b33507ad5ce41879970bf49c78aaaf7
user69gd <loganlloyd2374747474743747@gmail.com>
2025-09-15 15:54:32 -0500

    Create main_activity.java

---
+++ src/ui/android/main_activity.java
@@ -0,0 +1,12 @@
+// src/ui/android/main_activity.java
+package com.example.codeeditor;
+
+import android.os.Bundle;
+import org.qtproject.qt.android.bindings.QtActivity;
+
+public class MainActivity extends QtActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
